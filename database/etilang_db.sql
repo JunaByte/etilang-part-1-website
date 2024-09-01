71ed9f6060bf47f2700b8a4be6a271cadfd4cd2e
JunaByte <iamjunaa@gmail.com>
2024-09-02 05:01:52 +0800

    first commit

--- database/etilang_db.sql
+++ database/etilang_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 22, 2024 at 08:32 AM
+-- Generation Time: Sep 01, 2024 at 11:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
